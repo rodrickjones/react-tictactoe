d777a7772a62d415bc85dd063dff6bbd99a5514b
Rodrick Jones <rodrickgjones@gmail.com>
2020-08-25 12:32:44 +0930

    Simplified game and added styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,62 +6,42 @@ export default class Game extends Component {
         super(props);
         this.state = {
             xIsNext: true,
-            stepNumber: 0,
-            history: [
-                { squares: Array(9).fill(null) }
-            ]
+            squares: Array(9).fill(null)
         }
     }
 
-    jumpTo(step) {
+    resetGame(step) {
         this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0
+            xIsNext: true,
+            squares: Array(9).fill(null)
         })
     }
 
     handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
+        const squares = this.state.squares;
         if (squares[i] || calculateWinner(squares)) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            history: history.concat({
-                squares: squares
-            }),
             xIsNext: !this.state.xIsNext,
-            stepNumber: history.length
+            squares: squares
         });
     }
     
     render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-        const moves = history.map((step, move)=> {
-            const desc = move ? "Go to turn " + move : "Start game";
-            return (
-                <li key={move}> 
-                    <button onClick={()=>{this.jumpTo(move)}}>
-                        {desc}
-                    </button>
-                </li>
-
-            );
-        });
+        const squares = this.state.squares;
+        const winner = calculateWinner(squares);
         let status = winner ? winner + " wins!" : (this.state.xIsNext ? "X" : "O") + "'s turn";
         return (
             <div className="game">
+                <div className="status">{status}</div>
                 <div className="game-board">
-                    <Board squares={current.squares} onClick={(i)=> this.handleClick(i)} />
+                    <Board squares={squares} onClick={(i)=> this.handleClick(i)} />
                 </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ul>{moves}</ul>
-                </div>
+                <button className="reset-button" onClick={()=>{this.resetGame()}}>
+                    Reset Game
+                </button>
             </div>
         )
     }
